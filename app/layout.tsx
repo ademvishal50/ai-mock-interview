5ebaafa71c4e70f3642beb42374fc56ae1a37fcc
ademvishal06 <ademvishal06@gmail.com>
2025-08-14 11:42:02 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const mnoaSans = Mona_Sans({
+  variable: "--font-mono-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "An AI-powered platform for preparing for mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${mnoaSans.className} antialiased pattern`}
+      >
+        {children}
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
